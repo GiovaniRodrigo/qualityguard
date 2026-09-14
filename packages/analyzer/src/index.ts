bcc73c979bd925edaf1dcfcc7e3ab5128a1ff770
Giovani <109702583+GiovaniRodrigo@users.noreply.github.com>
2026-09-14 16:47:08 -0400

    feat(analyzer): expose public API

---
+++ packages/analyzer/src/index.ts
@@ -0,0 +1,4 @@
+export { analyze, calculateScore } from './analyzer.js';
+export { defaultRules, noInfrastructureImport } from './rules.js';
+export type { AnalyzeInput } from './analyzer.js';
+export type { Rule, SourceFile } from './rules.js';
